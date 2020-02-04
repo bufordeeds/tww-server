86182a6e6955d11d962e5b3a0fcbf4231da6e5a5
Buford Eeds <bufordeeds8@gmail.com>
2020-02-03 20:25:00 -0600

    gave all resources to customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :customers, only: [:create]
+      resources :customers
       post '/login', to: 'auth#create'
       get '/profile', to: 'customers#profile'
     end
