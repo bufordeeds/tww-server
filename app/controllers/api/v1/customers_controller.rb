147e5491dfcface5ba7a66ac53b09c5fbd71acee
Buford Eeds <bufordeeds8@gmail.com>
2020-02-03 20:24:39 -0600

    added index to customers controller

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,10 +1,16 @@
 class Api::V1::CustomersController < ApplicationController
-    skip_before_action :authorized, only: [:create]
+    skip_before_action :authorized, only: [:create, :index]
+
+    def index
+      @customers = Customer.all
+      render json: @customers
+    end
 
    def create
     @customer = Customer.create(customer_params)
     if @customer.valid?
-      render json: { customer: CustomerSerializer.new(@customer) }, status: :created
+      token = encode_token({ user_id: @customer.id })
+      render json: { customer: CustomerSerializer.new(@customer), jwt: token }, status: :created
     else
       render json: { error: 'failed to create customer' }, status: :not_acceptable
     end
