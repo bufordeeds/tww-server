1f4b7728d00029b66597e0cb2cb4ed181caf022e
Buford Eeds <bufordeeds8@gmail.com>
2020-02-04 22:19:09 -0600

    created seed data for menu item

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # buford = Customer.create(first_name: 'Buford', password: '123', last_name: 'Eeds', email: 'bufordeeds8@gmail.com', phone_number: '210-325-3989')
+
+# SIDES
+# gf = Item.create(name: 'Garlic Fries', description: 'Fries with garlic on top.', price: 6.00, item_type: 'side' )
+# ff = Item.create(name: 'French Fries', description: 'Classic french fries.', price: 5.00, item_type: 'side')
+# tots = Item.create(name: 'Tots', description: 'Classic tator tots.', price: 6.00, item_type: 'side' )
+# ranch = Item.create(name: 'Ranch', description: 'Classic buttermilk ranch.', price: 0.75 , item_type: 'side' )
+# bc = Item.create(name: 'Blue Cheese', description: 'Classic blue cheese', price: 0.75, item_type: 'side')
+
+# WINGS
+# hs = Item.create(name: '5 Piece Holy Schnikes', description: 'Painfully hot! ', price: 7, item_type: 'bone-in wing')
+# hsb = Item.create(name: '5 Piece Boneless Holy Schnikes', description: 'Painfully hot!', price: 6.5, item_type: 'bone-less wing')
+# b = Item.create(name: '5 Piece Buffalo', description: 'Traditional flavor but on the hot side.', price: 7, item_type: 'bone-in wing')
+# bb = Item.create(name: '4 Piece Boneless Buffalo', description: 'Traditional flavor but on the hot side.', price: 6.5, item_type: 'bone-less wing')
+# rof = Item.create(name: '5 Piece Ranch On Fire ', description: 'Dry rub, hot, and salty flavor.  . ', price: 7, item_type: 'bone-in wing')
+# rofb = Item.create(name: '4 Piece Boneless Ranch On Fire', description: 'Dry rub, hot, and salty flavor. ', price: 6.5, item_type: 'bone-less wing')
+# sp = Item.create(name: '5 Piece Spicey Pineapple', description: 'Sriracha based sauce with pineapple blended in.', price: 7, item_type: 'bone-in wing')
+# spb = Item.create(name: '4 Piece Boneless Spicey Pineapple', description: 'Sriracha based sauce with pineapple blended in.', price: 6.5, item_type: 'bone-less wing')
+# og = Item.create(name: '5 Piece OG', description: 'Sriracha based sauce so/so sweet.', price: 7, item_type: 'bone-in wing')
+# ogb = Item.create(name: '4 Piece Boneless OG', description: 'Sriracha based sauce so/so sweet.', price: 6.5, item_type: 'bone-less wing')
+# sc = Item.create(name: '5 Piece Sweet Chili', description: 'Mostly sweet with a kick.', price: 7, item_type: 'bone-in wing')
+# scb = Item.create(name: '4 Piece Boneless Sweet Chili', description: 'Mostly sweet with a kick.', price: 6.5, item_type: 'bone-less wing')
+# cl = Item.create(name: '5 Piece Chili Limon', description: 'Fresh lime juice and tajin seasoning', price: 7, item_type: 'bone-in wing')
+# clb = Item.create(name: '4 Piece Boneless Chili Limon', description: 'Fresh lime juice and tajin seasoning', price: 6.5, item_type: 'bone-less wing')
+# lp = Item.create(name: '5 Piece Lemon Pepper', description: 'Fresh lemon juice and cracked pepper.', price: 7, item_type: 'bone-in wing')
+# lpb = Item.create(name: '4 Piece Boneless Lemon Pepper', description: 'Fresh lemon juice and cracked pepper.', price: 6.5, item_type: 'bone-less wing')
+# gp = Item.create(name: '5 Piece Garlic Parm', description: 'Garlic butter and parmesean cheese', price: 7, item_type: 'bone-in wing')
+# gpb = Item.create(name: '4 Piece Boneless Garlic Parm', description: 'Garlic butter and parmesean cheese', price: 6.5, item_type: 'bone-less wing')
+
+# MERCH
+# redHat = Item.create(name: 'Red TWW Snapback', description: 'Rep your favorite wing joint with a snapback! ', price: 25, item_type: 'merch')
+# blackHat = Item.create(name: 'Black TWW Snapback', description: 'Rep your favorite wing joint with a snapback! ', price: 25, item_type: 'merch')
